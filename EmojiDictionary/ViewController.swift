0ed86439e7fcf8beb2231f58f36430c2c48b2aa3
James Richardson <james.richardson@invertedlight.com>
2020-05-09 18:57:46 -0700

    Added TableView and Emojis in array for each cell.

--- EmojiDictionary/ViewController.swift
+++ EmojiDictionary/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    var emojis = ["🔱", "😂", "😍","👾", "👄", "⚜️", "⛑", "👝", "👗", "🐒", "🦆", "🦖", "🐠", "🐊", "🚺", "💮", "📣", "🚩", "🎖", "🇺🇸", "♸"]
 
+    @IBOutlet weak var emojiTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+           return cell
+       }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        emojiTableView.dataSource = self
+        emojiTableView.delegate = self
+        
     }
 
 
