15ab24a34a6dedbc15fc84243f900dfca0414fa4
James Richardson <james.richardson@invertedlight.com>
2020-05-09 22:12:26 -0700

    Added two fields to Definition View for Category and Birthyear for each emoji

--- EmojiDictionary/DefinitionViewController.swift
+++ EmojiDictionary/DefinitionViewController.swift
@@ -12,6 +12,8 @@ class DefinitionViewController: UIViewController {
     
     var emoji = "No emoji"
     
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var birthYearLabel: UILabel!
     @IBOutlet weak var definitionLabel: UILabel!
     @IBOutlet weak var largeEmoji: UILabel!
     
@@ -25,69 +27,113 @@ class DefinitionViewController: UIViewController {
         switch(emoji) {
         case "😍" :
             definitionLabel.text = "Heart eyes smily face";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "🔱" :
             definitionLabel.text = "Trident pitch fork";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "😂" :
             definitionLabel.text = "Crying smily face";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "👾" :
             definitionLabel.text = "Alien face";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "👄" :
             definitionLabel.text = "Red lips";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "⚜️" :
             definitionLabel.text = "Flora De Lease";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "⛑" :
             definitionLabel.text = "Medic Helmet";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "👝" :
             definitionLabel.text = "Small Purse";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "👗" :
             definitionLabel.text = "Blue Dress";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "🐒" :
             definitionLabel.text = "Smiling Cute Monkey";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "🦆" :
             definitionLabel.text = "Malard Duck";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "🦖" :
             definitionLabel.text = "T-Rex Dinosour";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "🐠" :
             definitionLabel.text = "Blue Fin Angle Fish";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "🐊" :
             definitionLabel.text = "Smiling Alagator";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "🚺" :
             definitionLabel.text = "Woman Sign";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "💮" :
             definitionLabel.text = "Japanese Flower Stamp";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "📣" :
             definitionLabel.text = "Megaphone";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "🚩" :
             definitionLabel.text = "Red Flag";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "🎖" :
             definitionLabel.text = "Metal of Honor";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "🇺🇸" :
             definitionLabel.text = "USA Flag";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         case "♸" :
             definitionLabel.text = "Recycle #6 Sign";
+            categoryLabel.text = "Category: Animal";
+            birthYearLabel.text = "BirthYear: 2011";
             break;
         default :
-            definitionLabel.text = "Could not find a definition of that one"
+            definitionLabel.text = "Could not find a definition of that one";
+            categoryLabel.text = "Category: Unknown";
+            birthYearLabel.text = "BirthYear: Unknown";
         }
         
         
