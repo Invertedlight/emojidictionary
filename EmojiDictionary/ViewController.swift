f03bc42ff76b1596bee6ee28ca8dd236e14f4843
James Richardson <james.richardson@invertedlight.com>
2020-05-09 17:56:56 -0700

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by James Richardson on 5/9/20.
+//  Copyright © 2020 James Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
